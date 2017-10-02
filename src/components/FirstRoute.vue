63ece20a678ebc1bd7618c877563bead1efb5155
snake <lakhneche.sofiane@gmail.com>
2017-10-02 19:40:29 +0200

    Initial commit

---
+++ src/components/FirstRoute.vue
@@ -0,0 +1,18 @@
+<template>
+   <div>
+     <p>dededed</p>
+     <h1>{{msg}}</h1>
+     <a href="Hello">hello</a>
+       Hello {{ $route.params.name }}
+   </div>
+</template>
+<script>
+  export default {
+    name: 'firstroute',
+    data () {
+      return {
+        msg: 'FirstRoute'
+      }
+    }
+}
+</script>
